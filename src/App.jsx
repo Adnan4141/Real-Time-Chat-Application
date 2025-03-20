981b90ce5bb894dd71fa470f21677bf167e9a6b9
Adnan4141 <adnanh4141@gmail.com>
2025-03-21 03:49:07 +0600

    prod 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import "./app.css";
+import "./App.css";
 import MainRoute from "./routes/MainRoute";
 import { socket, SocketContext } from "./socket/socket";
 import { baseUrl } from "./utils/BaseUrl";
